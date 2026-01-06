793d56dfad186da82132810c04ca733815e33939
IkkonenG <ikkonengeorg@gmail.com>
2026-01-06 15:41:34 +0200

    Menu

--- Client/TTHK-Link/Pages/LoginPage.xaml.cs
+++ Client/TTHK-Link/Pages/LoginPage.xaml.cs
@@ -3,9 +3,15 @@ using TTHK_Link.ViewModels;
 
 public partial class LoginPage : ContentPage
 {
-	public LoginPage(LoginViewModel vm)
-	{
-		InitializeComponent();
-		BindingContext = vm;
-	}
+    public LoginPage(LoginViewModel vm)
+    {
+        InitializeComponent();
+        BindingContext = vm;
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        Shell.SetFlyoutBehavior(this, FlyoutBehavior.Disabled);
+    }
 }
