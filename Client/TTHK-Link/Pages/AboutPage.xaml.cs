f1ac04596cbcd2a2548f760ea2d525334580053e
IkkonenG <ikkonengeorg@gmail.com>
2026-01-06 21:18:51 +0200

    login page New

---
+++ Client/TTHK-Link/Pages/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTHK_Link.Pages;
+
+public partial class AboutPage : ContentPage
+{
+    public AboutPage()
+    {
+        InitializeComponent();
+    }
+}
